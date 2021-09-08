221d5532280c33d9a81e77b5d833bdcd549e3168
Thong <thong@gmail.com>
2021-09-08 16:23:14 +0700

    them profile

--- app/Http/Controllers/AccountController.php
+++ app/Http/Controllers/AccountController.php
@@ -2,11 +2,31 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Account;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Auth;
 
 class AccountController extends Controller
 {
     public function index(){
         return view('account.profile');
+    }
+    public function store(Request $request)
+    {
+        try{
+            $account = new Account;
+            $account->username = Auth::user()->username;
+            $account->fullname  = $request->fullname;
+            $account->doB  = $request->doB;
+            $account->emails = $request->emails;
+            $account->sđt = $request->sđt;
+            $account->gender = $request->gender;
+            $account->save();
+            return redirect()->route('seller.storeAccount')->with('success', 'them thanh cong');
+        }catch(Exception $error){
+            return redirect()->route('seller.storeAccount')->with('error', 'loi');
+        }
+
     }
 }
